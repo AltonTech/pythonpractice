6fbd453cad6ab54c8df4e8093cf7b46b91cef9c9
J <Jessealton23@gmail.com>
2017-07-02 15:20:35 -0400

    Add file via upload
    brief python practice program

---
+++ helloworld.py
@@ -0,0 +1,6 @@
+personsname = raw_input("Please type your name and press enter: ")
+personsage = raw_input("How old are you?")
+print("hello " + personsname + ", you are " + personsage + " years old ")
+personsspouse = raw_input("Who do you love?")
+print(personsname + ", you and " + personsspouse + " will find happiness together!")
+
